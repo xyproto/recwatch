7a021bdcf3846de0c7f4b4888e33da071f64acf2
Alexander F Rødseth <rodseth@gmail.com>
2017-09-28 13:13:29 +0200

    Add stub version for gccgo

--- recwatch_gccgo.go
+++ recwatch_gccgo.go
@@ -1,60 +1,26 @@
 // Package recwatch provides a way to watch directories recursively with fsnotify
 
-// +build !gccgo
+// +build gccgo
 
 package recwatch
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/fsnotify/fsnotify"
 )
 
-type (
-	Event fsnotify.Event
-
-	RecursiveWatcher struct {
-		*fsnotify.Watcher
-		Files   chan string
-		Folders chan string
-	}
-)
-
-func (e Event) String() string {
-	return fsnotify.Event(e).String()
+type RecursiveWatcher struct {
+	_       interface{}
+	Files   chan string
+	Folders chan string
 }
 
 func NewRecursiveWatcher(path string) (*RecursiveWatcher, error) {
-	folders := Subfolders(path)
-	if len(folders) == 0 {
-		return nil, errors.New("No folders to watch.")
-	}
-
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, err
-	}
-	rw := &RecursiveWatcher{Watcher: watcher}
-
-	rw.Files = make(chan string, 10)
-	rw.Folders = make(chan string, len(folders))
-
-	for _, folder := range folders {
-		if err = rw.AddFolder(folder); err != nil {
-			return nil, err
-		}
-	}
-	return rw, nil
+	return nil, nil
 }
 
 func (watcher *RecursiveWatcher) AddFolder(folder string) error {
-	if err := watcher.Add(folder); err != nil {
-		return err
-	}
-	watcher.Folders <- folder
 	return nil
 }
 
